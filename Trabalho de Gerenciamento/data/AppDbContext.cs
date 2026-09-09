7f88552811502d4e22e0b95b263ac4933f2147f3
Tittozo <mateusetec424@gmail.com>
2026-09-09 20:27:35 -0300

    feat: criar AppDbContext

---
+++ Trabalho de Gerenciamento/data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Trabalho_de_Gerenciamento.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
