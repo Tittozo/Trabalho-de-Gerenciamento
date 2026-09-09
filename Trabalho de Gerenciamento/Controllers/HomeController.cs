bb776b153e739008bfe80e70709c26fc50eef531
Tittozo <mateusetec424@gmail.com>
2026-09-09 20:16:20 -0300

    chore: criar projeto inicial

---
+++ Trabalho de Gerenciamento/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Trabalho_de_Gerenciamento.Models;
+
+namespace Trabalho_de_Gerenciamento.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Trabalho de Gerenciamento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Trabalho_de_Gerenciamento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
