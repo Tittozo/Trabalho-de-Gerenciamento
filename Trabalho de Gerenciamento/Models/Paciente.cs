abafd114f4833860da8c1a8470432a6b5dce40e4
Tittozo <mateusetec424@gmail.com>
2026-09-09 21:21:03 -0300

    feat: adicionar validações em Paciente

--- Trabalho de Gerenciamento/Models/Paciente.cs
+++ Trabalho de Gerenciamento/Models/Paciente.cs
@@ -1,6 +1,22 @@
-﻿namespace Trabalho_de_Gerenciamento.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Trabalho_de_Gerenciamento.Models;
+
+public class Paciente
 {
-    public class Paciente
-    {
-    }
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string CPF { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        public string Endereco { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
 }
