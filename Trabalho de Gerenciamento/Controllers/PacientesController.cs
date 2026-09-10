ffb13535c3d3c1e67e6b2ddb0296b50e7676ab28
Tittozo <mateusetec424@gmail.com>
2026-09-10 14:18:35 -0300

    feat: listar pacientes

---
+++ Trabalho de Gerenciamento/Controllers/PacientesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trabalho_de_Gerenciamento.Data;
+
+namespace Trabalho_de_Gerenciamento.Controllers
+{
+    public class PacientesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PacientesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Pacientes);
+        }
+    }
+}
