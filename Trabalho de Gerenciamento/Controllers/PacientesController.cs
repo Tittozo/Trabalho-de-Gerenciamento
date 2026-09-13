7b479b08b819e0f35c893807bc504f181597378f
Tittozo <mateusetec424@gmail.com>
2026-09-13 17:50:28 -0300

    feat: editar pacientes

--- Trabalho de Gerenciamento/Controllers/PacientesController.cs
+++ Trabalho de Gerenciamento/Controllers/PacientesController.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Trabalho_de_Gerenciamento.Data;
 using Trabalho_de_Gerenciamento.Models;
 
@@ -31,8 +30,46 @@ namespace Trabalho_de_Gerenciamento.Controllers
             {
                 _context.Pacientes.Add(paciente);
                 _context.SaveChanges();
+
                 return RedirectToAction("Index");
             }
+
+            return View(paciente);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = _context.Pacientes.Find(id);
+
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Pacientes paciente)
+        {
+            if (id != paciente.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Pacientes.Update(paciente);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
             return View(paciente);
         }
     }
